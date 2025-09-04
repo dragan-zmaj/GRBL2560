13681211e736eab4f119167522f4a9e306d51ae1
Luka Lopin <lukabmx69@gmail.com>
2025-09-04 19:41:34 +0200

    Add definition of ATMEGA2560 controller to config.h, entry for new controller is made in cpu_map.h, it remains to change registry names.

--- grbl/config.h
+++ grbl/config.h
@@ -35,7 +35,8 @@
 // one configuration file by placing their specific defaults and pin map at the bottom of this file.
 // If doing so, simply comment out these two defines and see instructions below.
 #define DEFAULTS_GENERIC
-#define CPU_MAP_ATMEGA328P // Arduino Uno CPU
+#define CPU_MAP_ATMEGA2560
+//#define CPU_MAP_ATMEGA328P // Arduino Uno CPU
 
 // Serial baud rate
 // #define BAUD_RATE 230400
